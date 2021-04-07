7850ded2e83cb7a4e952307aff96d46ae0524566
Taylor Gonzales <hello@taylorgonz.com>
2021-04-07 18:13:32 -0400

    mongo activities

---
+++ 01-Activities/06-Stu-MongoJS-Sorting/Unsolved/server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongojs = require("mongojs");
+
+const app = express();
+
+const databaseUrl = "mongodb+srv://taylorgonz-admin:vCyowWOt2NlG58Hh@cluster0.djs8r.mongodb.net/zoo?retryWrites=true&w=majority";
+const collections = ["animals"];
+
+
+const db = mongojs(databaseUrl, collections);
+
+
+
+
+
+
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/all", (req, res) => {
+  db.animals.find({}, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+// TODO: Implement the remaining two routes
+
+// 1: Name: Send JSON response sorted by name in ascending order, e.g. GET "/name"
+
+// 2: Weight: Send JSON response sorted by weight in descending order, , e.g. GET "/weight"
+
+// Set the app to listen on port 3000
+app.listen(3000, () => {
+  console.log("App running on port 3000!");
+});
